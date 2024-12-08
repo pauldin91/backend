e4d7819503a270b96338fbf2c6e33809b9a7fa84
pauldin91 <dinopoul@csd.auth.gr>
2024-12-08 18:26:00 +0200

    add patch for user

---
+++ gapi/rpc_update_user.go
@@ -0,0 +1,88 @@
+package gapi
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/jackc/pgx/v5/pgtype"
+	db "github.com/pauldin91/backend/db/sqlc"
+	pb "github.com/pauldin91/backend/pb"
+	"github.com/pauldin91/backend/utils"
+	validator "github.com/pauldin91/backend/validation"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (server *Server) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error) {
+	violations := validateUpdateUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
+	arg := db.UpdateUserParams{
+		Username: req.GetUsername(),
+		FullName: pgtype.Text{
+			String: req.GetFullName(),
+			Valid:  req.FullName != nil,
+		},
+		Email: pgtype.Text{
+			String: req.GetEmail(),
+			Valid:  req.Email != nil,
+		},
+	}
+	if req.Password != nil {
+
+		hashedPassword, err := utils.HashPassword(req.GetPassword())
+		if err != nil {
+			return nil, status.Errorf(codes.Unimplemented, "method unimplemented")
+		}
+
+		arg.HashedPassword = pgtype.Text{
+			String: hashedPassword,
+			Valid:  true,
+		}
+	}
+
+	user, err := server.store.UpdateUser(ctx, arg)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, status.Errorf(codes.NotFound, "user : %s not found", err)
+
+		}
+		return nil, status.Errorf(codes.Internal, "failed to update user : %s", err)
+	}
+
+	rsp := &pb.UpdateUserResponse{
+		User: convertUser(user),
+	}
+
+	return rsp, nil
+}
+
+func validateUpdateUserRequest(req *pb.UpdateUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := validator.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+
+	if req.Password != nil {
+		if err := validator.ValidatePassword(req.GetPassword()); err != nil {
+			violations = append(violations, fieldViolation("password", err))
+		}
+	}
+
+	if req.FullName != nil {
+		if err := validator.ValidateFullname(req.GetFullName()); err != nil {
+			violations = append(violations, fieldViolation("full_name", err))
+		}
+	}
+
+	if req.Email != nil {
+		if err := validator.ValidateEmail(req.GetEmail()); err != nil {
+			violations = append(violations, fieldViolation("email", err))
+		}
+	}
+
+	return violations
+}
